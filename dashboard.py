2848c02588e0494b4db4cea8d9a5f5dab762f95a
Ammar Ash shiddiq <Ammarashshiddiq2@gmail.com>
2023-07-19 23:22:43 +0700

    fix dashboard

---
+++ dashboard.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+st.title('Portland Trail Blazers Performance Analysis')
+st.subheader('By Ammar Ash Shiddiq')
+
+
+st.markdown("---")
+
+st.header('Background')
+"""I will be performing a simple web scraping and perform some data analysis
+Since I am a portland trailblazer fan, I want to see how well is my team last year. 
+The data are taken from https://www.basketball-reference.com/. 
+The data are official from the NBA so we don't have to worry about some data error."""
